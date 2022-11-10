91b2a6c7896753b772c5757e471bcac144acd1ae
Vinay <navik.vinay12@gmail.com>
2022-11-11 02:32:42 +0530

    calcualted 20 days wage

--- OOPEmpWageComputation/OOPEmpWage.cs
+++ OOPEmpWageComputation/OOPEmpWage.cs
@@ -5,37 +5,39 @@
         const int IS_FULL_TIME = 1;
         const int IS_PART_TIME = 2;
         const int EMP_RATE_PER_HOUR = 20;
+        const int NUM_OF_WORKING_DAYS = 20;
         public static void Welcome()
         {
             Console.WriteLine("Welcome to EmployeeWage Computation Program");
         }
-        public static void EmpWagePerDay()
+        public static void EmpWage20Day()
         {
-            int empHrs = 0, empWage = 0;
-            Random random = new Random();
-            int empCheck = random.Next(0, 3);
-            switch (empCheck)
+            int empHrs = 0, empWage = 0, TotalEmpWage = 0;
+            for (int days = 1; days <= NUM_OF_WORKING_DAYS; days++)
             {
-                case IS_FULL_TIME:
-                    Console.WriteLine("Full Time Employee is Present");
-                    empHrs = 8;
-                    break;
-                case IS_PART_TIME:
-                    Console.WriteLine("Part Time Employee is Present");
-                    empHrs = 4;
-                    break;
-                default:
-                    Console.WriteLine("Employee is Absent");
-                    empHrs = 0;
-                    break;
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+                switch (empCheck)
+                {
+                    case IS_FULL_TIME:
+                        empHrs = 8;
+                        break;
+                    case IS_PART_TIME:
+                        empHrs = 4;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                empWage = EMP_RATE_PER_HOUR * empHrs;
+                TotalEmpWage += empWage;
             }
-            empWage = EMP_RATE_PER_HOUR * empHrs;
-            Console.WriteLine("Employee Wage : " + empWage);
+            Console.WriteLine("Employee Wage for 20 Days is : " + TotalEmpWage);
         }
         public static void Main(string[] args)
         {
             Welcome();
-            EmpWagePerDay();
+            EmpWage20Day();
         }
     }
 }
