47a4730fd1861403d0dcd632a1ddf0dbaf8f0866
Vinay <navik.vinay12@gmail.com>
2022-11-11 02:21:12 +0530

    added part time feature

--- OOPEmpWageComputation/OOPEmpWage.cs
+++ OOPEmpWageComputation/OOPEmpWage.cs
@@ -3,6 +3,7 @@
     internal class OOPEmpWage
     {
         const int IS_FULL_TIME = 1;
+        const int IS_PART_TIME = 2;
         const int EMP_RATE_PER_HOUR = 20;
         public static void Welcome()
         {
@@ -12,11 +13,16 @@
         {
             int empHrs = 0, empWage = 0;
             Random random = new Random();
-            int empCheck = random.Next(0, 2);
+            int empCheck = random.Next(0, 3);
             if (empCheck == IS_FULL_TIME)
             {
-                Console.WriteLine("Employee is Present");
+                Console.WriteLine("Full Time Employee is Present");
                 empHrs = 8;
+            }
+            else if (empCheck == IS_PART_TIME)
+            {
+                Console.WriteLine("Part Time Employee is Present");
+                empHrs = 4;
             }
             else
             {
