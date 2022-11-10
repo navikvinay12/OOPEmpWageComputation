2d0e1b89e8e59c18b8e2c93c1699020f2a8f61bf
Vinay <navik.vinay12@gmail.com>
2022-11-11 02:10:34 +0530

    added method to check emp present or absent

--- OOPEmpWageComputation/OOPEmpWage.cs
+++ OOPEmpWageComputation/OOPEmpWage.cs
@@ -2,13 +2,28 @@
 {
     internal class OOPEmpWage
     {
+        const int IS_FULL_TIME = 1;
         public static void Welcome()
         {
             Console.WriteLine("Welcome to EmployeeWage Computation Program");
         }
-        static void Main(string[] args)
+        public static void EmpPresentAbsent()
+        {
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            if (empCheck == IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+        }
+        public static void Main(string[] args)
         {
             Welcome();
+            EmpPresentAbsent();
         }
     }
 }
