b7a0642c07cad3cb5ce616bed7a0b38ee9f31c3a
Vinay <navik.vinay12@gmail.com>
2022-11-11 02:16:34 +0530

    added method to calculate empWage per day

--- OOPEmpWageComputation/OOPEmpWage.cs
+++ OOPEmpWageComputation/OOPEmpWage.cs
@@ -3,27 +3,33 @@
     internal class OOPEmpWage
     {
         const int IS_FULL_TIME = 1;
+        const int EMP_RATE_PER_HOUR = 20;
         public static void Welcome()
         {
             Console.WriteLine("Welcome to EmployeeWage Computation Program");
         }
-        public static void EmpPresentAbsent()
+        public static void EmpWagePerDay()
         {
+            int empHrs = 0, empWage = 0;
             Random random = new Random();
             int empCheck = random.Next(0, 2);
             if (empCheck == IS_FULL_TIME)
             {
                 Console.WriteLine("Employee is Present");
+                empHrs = 8;
             }
             else
             {
                 Console.WriteLine("Employee is Absent");
+                empHrs = 0;
             }
+            empWage = EMP_RATE_PER_HOUR * empHrs;
+            Console.WriteLine("Employee Wage : " + empWage);
         }
         public static void Main(string[] args)
         {
             Welcome();
-            EmpPresentAbsent();
+            EmpWagePerDay();
         }
     }
 }
