3e0355abace6f3ef49aedaa4ed22974ad6abdd84
Vinay <navik.vinay12@gmail.com>
2022-11-11 01:55:32 +0530

    welcome msg

---
+++ OOPEmpWageComputation/OOPEmpWage.cs
@@ -0,0 +1,14 @@
+﻿namespace OOPEmpWageComputation
+{
+    internal class OOPEmpWage
+    {
+        public static void Welcome()
+        {
+            Console.WriteLine("Welcome to EmployeeWage Computation Program");
+        }
+        static void Main(string[] args)
+        {
+            Welcome();
+        }
+    }
+}
